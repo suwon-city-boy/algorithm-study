36fc88184373fd54826fd44d899a98be03025b6c
Kim Sung Kyu <77334798+suwon-city-boy@users.noreply.github.com>
2021-01-21 15:38:31 +0900

    BFS(수정됨)
    BFS 안에서 단지번호 별 단지 개수를 저장하도록 변경

--- [BOJ]2667.cpp
+++ [BOJ]2667.cpp
@@ -1,59 +1,67 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
 #include <algorithm>
 using namespace std;
+int ans[25*25];
 int a[25][25];
-int group[25][25];
-int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-int ans[25 * 25];
+int d[25][25];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 int n;
-void bfs(int i, int j, int cnt) {
-	queue<pair<int, int>> q;
-	q.push(make_pair(i, j));
-	group[i][j] = cnt;
-	while (!q.empty()) {
-		i = q.front().first;
-		j = q.front().second;
-		q.pop();
-		for (int k = 0; k < 4; k++) {
-			int nx = i + dx[k];
-			int ny = j + dy[k];
-			if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-				if (a[nx][ny] == 1 && group[nx][ny] == 0) {
-					q.push(make_pair(nx, ny));
-					group[nx][ny] = cnt;
-				}
-			}
-		}
-	}
+void bfs(int x, int y, int cnt)
+{
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    d[x][y] = cnt;
+    ans[cnt]++;
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0 <= nx && nx < n && 0 <= ny && ny < n)
+            {
+                if(a[nx][ny] == 1 && d[nx][ny] == 0)
+                {
+                    q.push(make_pair(nx, ny));
+                    d[nx][ny] = cnt;
+                    ans[cnt]++;
+                }
+            }
+        }
+    }
 }
 
-int main() {
-	cin >> n;
+int main()
+{
+    scanf("%d", &n);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%1d", &a[i][j]);
-		}
-	}
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%1d", &a[i][j]);
+        }
+    }
 
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (a[i][j] == 1 && group[i][j] == 0)
-				bfs(i, j, ++cnt);
-		}
-	}
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			ans[group[i][j]] += 1;
-		}
-	}
-	cout << cnt << "\n";
-	sort(ans + 1, ans + cnt + 1);
-	for (int i = 1; i <= cnt; i++) {
+    int cnt = 0;  
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i][j] == 1 && d[i][j] == 0)
+                bfs(i, j, ++cnt);
+        }
+    }
+
+    printf("%d\n", cnt);
+    sort(ans+1, ans+cnt+1);
+    for (int i = 1; i <= cnt; i++) {
 		printf("%d\n", ans[i]);
 	}
 }
