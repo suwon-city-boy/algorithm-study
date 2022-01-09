40cc4a048c1d7942fcf792b050d6fa6fd38b730d
Kim Sung Kyu <96288439+suwon-city-boy@users.noreply.github.com>
2022-01-09 21:36:43 +0900

    2022/01/09

---
+++ 인프런 강의/2. 정렬 & 이분탐색 & 투포인트 & 스택/교집합(투포인터 알고리즘).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m, p1 = 0, p2 = 0, p3 = 0;
+	scanf("%d", &n);
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	sort(a.begin(), a.end());
+	scanf("%d", &m);
+	vector<int> b(n), c(n+m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &b[i]);
+	}
+	sort(b.begin(), b.end());
+	while (p1 < n && p2 < m) {
+		if (a[p1] < b[p2]) {
+			p1++;
+		}
+		else if(a[p1] < b[p2]){
+			p2++;
+		}
+		else {
+			c[p3++] = a[p1++];
+			p2++;
+		}
+	}
+	for (int i = 0; i < p3; i++) {
+		printf("%d ", c[i]);
+	}
+	return 0;
+}
